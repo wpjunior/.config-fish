840b51f33c3e2fb382a9bd558c58ac984f92f91f
Wilson Júnior <wilsonpjunior@gmail.com>
2021-08-24 10:52:54 -0300

    Update to mac environment

--- config.fish
+++ config.fish
@@ -1,8 +1,20 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/home/wilson/.local/share/omf"
+set -gx OMF_PATH "/Users/wilson.junior/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "/Users/wilson.junior/.config/omf"
+
+# homebrew
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+
+# gpg fix
+set -x GPG_TTY (tty)
 
 # virtualfish
 set PROJECT_HOME $HOME/Globo
@@ -11,11 +23,11 @@ set VIRTUALFISH_COMPAT_ALIASES
 set GREP_OPTIONS '--color=always'
 set -g theme_display_k8s_context yes
 
-source /usr/lib/python3.6/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.6/site-packages/virtualfish/auto_activation.fish
-source /usr/lib/python3.6/site-packages/virtualfish/compat_aliases.fish
-source /usr/lib/python3.6/site-packages/virtualfish/global_requirements.fish
-source /usr/lib/python3.6/site-packages/virtualfish/projects.fish
+source /Users/wilson.junior/Library/Python/3.8/lib/python/site-packages/virtualfish/virtual.fish
+source /Users/wilson.junior/Library/Python/3.8/lib/python/site-packages/virtualfish/auto_activation.fish
+source /Users/wilson.junior/Library/Python/3.8/lib/python/site-packages/virtualfish/compat_aliases.fish
+source /Users/wilson.junior/Library/Python/3.8/lib/python/site-packages/virtualfish/global_requirements.fish
+source /Users/wilson.junior/Library/Python/3.8/lib/python/site-packages/virtualfish/projects.fish
 source $HOME/.config/fish/completions/kubectl_aliases.fish
 emit virtualfish_did_setup_plugins
 
@@ -28,6 +40,8 @@ alias pbcopy='xclip -i'
 alias stg='git diff --staged'
 alias cat='bat'
 alias du='ncdu'
+alias tf='terraform'
+alias tffmt='terraform fmt -recursive'
 alias curl-latency='curl -w "@/home/wilson/.config/curl-latency.txt" -o /dev/null -s'
 alias icat='kitty +kitten icat'
 alias tprod='tsuru --target=https://tsuru.globoi.com'
@@ -37,10 +51,13 @@ alias tlab='tsuru --target=https://lab.tsuru.globoi.com'
 set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin $PATH
 
-set -x EDITOR emacsclient
+# mac stuff
+set -x PATH /usr/local/bin $PATH
+set -x PATH /opt/homebrew/opt/coreutils/libexec/gnubin $PATH
+set -x PATH /opt/homebrew/opt/gnu-sed/libexec/gnubin $PATH
+set -x PATH /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/ $PATH
 
-# my personal bins
-set -x PATH $HOME/.bin $PATH
+set -x EDITOR emacsclient
 
 # Load oh-my-fish configuration.
 
@@ -60,4 +77,4 @@ if test -e .node-version
     nvm use --silent (cat .node-version)
 end
 
-kitty + complete setup fish | source
+# kitty + complete setup fish | source
